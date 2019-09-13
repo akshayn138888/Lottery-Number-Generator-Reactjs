09b2b079ee00ff414cebed7973d48c92d90b2f13
akshayn138888 <akshayn@sfu.ca>
2019-09-12 17:43:01 -0700

    All files attached

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+import Ball from './Ball'
+import Lottery from './Lottery'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Lottery/>
+      <Lottery title = 'BC-Daily' maxNum = {10} numBalls = {4}/>
+
     </div>
   );
 }
